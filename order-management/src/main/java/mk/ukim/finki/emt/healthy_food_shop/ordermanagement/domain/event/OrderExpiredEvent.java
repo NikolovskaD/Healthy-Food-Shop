3257cdb629cf9a232373ec63c034ba3f64f2e4a3
Jelena <jelena.mikan95@gmail.com>
2020-07-10 23:55:23 +0200

    fix events

--- order-management/src/main/java/mk/ukim/finki/emt/healthy_food_shop/ordermanagement/domain/event/OrderExpiredEvent.java
+++ order-management/src/main/java/mk/ukim/finki/emt/healthy_food_shop/ordermanagement/domain/event/OrderExpiredEvent.java
@@ -1,9 +1,34 @@
 package mk.ukim.finki.emt.healthy_food_shop.ordermanagement.domain.event;
 
-public class OrderExpiredEvent {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import mk.ukim.finki.emt.healthy_food_shop.sharedkernel.domain.base.DomainEvent;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class OrderExpiredEvent implements DomainEvent {
+    private final LocalDateTime when;
     public String orderId;
 
-    public OrderExpiredEvent(String orderId) {
+    @JsonProperty("occurredOn")
+    private final Instant occurredOn;
+
+    public OrderExpiredEvent(String orderId, Instant occurredOn) {
+        this.occurredOn = occurredOn;
+        this.when = LocalDateTime.now();
         this.orderId = orderId;
+    }
+
+    public LocalDateTime getWhen() {
+        return when;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    @Override
+    public Instant occurredOn() {
+        return occurredOn;
     }
 }
--- product-catalog/src/main/java/mk/ukim/finki/emt/healthy_food_shop/productcatalog/domain/model/ProductId.java
+++ product-catalog/src/main/java/mk/ukim/finki/emt/healthy_food_shop/productcatalog/domain/model/ProductId.java
@@ -1,5 +1,6 @@
 package mk.ukim.finki.emt.healthy_food_shop.productcatalog.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import mk.ukim.finki.emt.healthy_food_shop.sharedkernel.domain.base.DomainObjectId;
 
 import javax.persistence.Embeddable;
@@ -7,9 +8,13 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class ProductId extends DomainObjectId {
 
-    private ProductId() { super(""); }
+    private ProductId() {
+        super(DomainObjectId.randomId(ProductId.class).toString());
+    }
 
+    @JsonCreator
     public ProductId(String id) {
         super(id);
     }
+
 }
